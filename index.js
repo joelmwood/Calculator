b48e74f07cce982da2c85578ea6e69a2fd18946a
Joel Wood <joelmwood@live.com>
2022-12-28 04:09:10 -0600

    Added basic math functions to JS

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a, b){
+    return a+b;
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a / b;
+}
+
+function operate(operator, a, b){
+    if(operator === "+"){
+        return a + b;
+    }
+    if(operator === "-"){
+        return a - b;
+    }
+    if(operator === "*"){
+        return a * b;
+    }
+    if(operator === "/"){
+        return a / b;
+    }
+}
